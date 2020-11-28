628c72b4d67c7d01cc2532bececf8d8d5cbd86fb
hamouda-bh <benhamouda.mohamed@esprit.tn>
2020-11-28 01:47:54 +0100

    merging everything

---
+++ BookStore/src/bookstore/Testing/MainProg.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore.Testing;
+
+import bookstore.entities.Client;
+import bookstore.services.ClientService;
+
+/**
+ *
+ * @author PC-Hamouda
+ */
+public class MainProg {
+    public static void main(String[] args) {
+        ClientService cs = new ClientService();
+        Client c1 = new Client( "qsdazdsqsdsqqfdsqfqss", "Ben qsqdqsd", "qsdqsdq", "qsdqdqsd.mohamed@esprit.tn","qsqdsqdqs", "C:/Users/Photo.jpg");
+        System.out.println(c1.toString()); 
+        cs.ajouterClientDB(c1);
+        
+    }
+}
