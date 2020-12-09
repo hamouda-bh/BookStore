0884b38782f2aa3164202dd8ac67c620ec7eeae6
hamouda-bh <benhamouda.mohamed@esprit.tn>
2020-12-09 18:46:38 +0100

    jar files

--- src/bookstore/views/ViewFactory.java
+++ src/bookstore/views/ViewFactory.java
@@ -6,11 +6,8 @@
 
 package bookstore.views;
 
-import bookstore.viewsControllers.LivreKidsController;
-import bookstore.viewsControllers.BaseController;
-import bookstore.viewsControllers.CommandeFaiteController;
-import bookstore.viewsControllers.GererPanierController;
-import bookstore.viewsControllers.PasserCommandeController;
+import bookstore.viewsControllers.*;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -50,23 +47,59 @@ public class ViewFactory {
 	    }
     
     
+        public void initializeStage(BaseController baseController) {
+	    	
+	    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
+	        fxmlLoader.setController(baseController);
+	        Parent parent;
+	        try {
+	            parent = fxmlLoader.load();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            return;
+	        }
+	        Scene scene = new Scene(parent);
+	        Stage stage = new Stage();
+	        
+	        stage.setScene(scene);
+	        stage.show();
+	        st.add(stage);
+	    }
+    public void showCategoriesWindowFromLivre(){
+        BaseController cat = new LivreKidsController(this, "CategorieKidsView.fxml");
+        initializeStage(cat);
+    }
     public void displayCategories(){
         
     }
     
-    public void showCommandeForm(  )
-    {
-        BaseController b= new GererPanierController(this, "PasserCommande.fxml");
-         initializeStage(b);
-    }
-     public void showPanier(  )
-    {
-        BaseController b2= new PasserCommandeController(this, "GererPanier.fxml");
-         initializeStage(b2);
-    }
-     public void showCommandeFaite()
-    {
-        BaseController b3= new CommandeFaiteController(this, "commandeFaite.fxml");
-         initializeStage(b3);
-    }
+    public void showLoginWindow() {
+	        System.out.println("show login window called");
+			BaseController loginController = new LoginWindowController(this, "LoginWindow.fxml");
+			initializeStage(loginController);
+	    }
+	    public void showRegisterWindow() {
+	        System.out.println("show register window called");
+			BaseController registerController = new RegisterWindowController(this, "RegisterWindowController.fxml");
+			initializeStage(registerController);
+	    }
+	    public void showMainWindow() {
+	        System.out.println("show main window called");
+			BaseController mainController = new MainWindowController(this, "MainWindowView.fxml");
+			initializeStage(mainController);
+	    }
+	    public void closeStage(Stage stageToClose) {
+	    	stageToClose.close();
+	    	st.remove(stageToClose);
+	    }
+	    public void showAccountWindow(){
+	    	System.out.println("show account window called");
+			BaseController accountController = new AccountWindowController(this, "AccountWindow.fxml");
+			initializeStage(accountController);
+	    }
+	    public void showAccountEditWindow(){
+	    	System.out.println("show account edit window called");
+			BaseController editAccountController = new EditAccountController(this, "EditAccountWindow.fxml");
+			initializeStage(editAccountController);
+	    }
 }
