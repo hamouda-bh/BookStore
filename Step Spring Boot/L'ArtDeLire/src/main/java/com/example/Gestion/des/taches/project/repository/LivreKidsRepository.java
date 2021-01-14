cd55e9ebe8caf25e154028b618d83f26a995c0c0
hamouda-bh <benhamouda.mohamed@esprit.tn>
2021-01-14 22:49:19 +0100

    creating livreKidsRepository interface

---
+++ Step Spring Boot/L'ArtDeLire/src/main/java/com/example/Gestion/des/taches/project/repository/LivreKidsRepository.java
@@ -0,0 +1,10 @@
+package com.example.Gestion.des.taches.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Gestion.des.taches.project.model.LivreKids;
+
+public interface LivreKidsRepository extends JpaRepository<LivreKids, Long>{
+
+}
+
