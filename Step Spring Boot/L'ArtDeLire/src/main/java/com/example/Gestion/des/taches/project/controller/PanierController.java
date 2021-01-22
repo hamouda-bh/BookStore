4984629f3ed133dc079fede8af8f60e1b9a8576c
nour <nour.benhadjyahia@esprit.tn>
2021-01-22 22:58:43 +0100

    iiiaaa

--- Step Spring Boot/L'ArtDeLire/src/main/java/com/example/Gestion/des/taches/project/controller/PanierController.java
+++ Step Spring Boot/L'ArtDeLire/src/main/java/com/example/Gestion/des/taches/project/controller/PanierController.java
@@ -1,25 +1,16 @@
 package com.example.Gestion.des.taches.project.controller;
 
 import java.util.Optional;
-
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.example.Gestion.des.taches.project.model.Commande;
 import com.example.Gestion.des.taches.project.model.Livre;
 import com.example.Gestion.des.taches.project.model.Panier;
-import com.example.Gestion.des.taches.project.model.User;
-import com.example.Gestion.des.taches.project.service.CommandeService;
-import com.example.Gestion.des.taches.project.service.LivreService;
 import com.example.Gestion.des.taches.project.service.PanierService;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
